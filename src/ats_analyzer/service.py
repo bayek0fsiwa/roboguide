dea2a4e01334faab88848cb565bac68d5623031c
bayek0fsiwa <imvikas@tutanota.com>
2025-04-05 14:15:31 +0530

    fix analyzer

--- src/ats_analyzer/service.py
+++ src/ats_analyzer/service.py
@@ -1,6 +1,9 @@
 import os
+import re
 from google import genai
 from dotenv import load_dotenv
+from pypdf import PdfReader
+
 
 load_dotenv()
 GEMINI_KEY: str = os.getenv("GEMINI_KEY")
@@ -8,15 +11,21 @@ client = genai.Client(api_key=GEMINI_KEY)
 
 
 def analyze_resume(resume_path: str):
-    contents = ""
-    with open(resume_path, "r") as f:
-        contents = f.read()
+    reader = PdfReader(resume_path)
+    page = reader.pages[0]
+    contents = page.extract_text()
+    # contents = ""
+    # with open(resume_path, "rb") as f:
+    #     contents = f.read()
     prompt = """
     you are a ATS resume analyzer. You will receive content from resume and
     you need to analyze and suggest the improvements and what could have been done better.
+    and give response in json format.
     """
+    # uploaded_file = client.files.upload(file=resume_path)
     response = client.models.generate_content(
         model="gemini-2.0-flash", contents=f"{prompt}: Here is the content to analyze - {contents}"
     )
-
+    result = response.text
+    re.sub("\n  ", "", result)
     return response.text

b1084b585d7b0f79059cda6da0d970c7f839b272
bayek0fsiwa <imvikas@tutanota.com>
2025-04-12 22:48:34 +0530

    add pydantic setting

---
+++ src/config/configs.py
@@ -0,0 +1,29 @@
+import pathlib
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = BASE_DIR = pathlib.Path(__file__).parent.parent.parent
+ENV_DIR = BASE_DIR / ".env"
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str = ""
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+    DATABASE_URL: str = ""
+    REDIS_HOST: str = ""
+    REDIS_PORT: int
+    PORT: int
+    GEMINI_KEY: str = ""
+    GEMINI_URL: str = ""
+    AWS_ACCESS_KEY: str = ""
+    AWS_SECRET_KEY: str = ""
+    COGNITO_CLIENT_NAME: str = ""
+    COGNITO_CLIENT_ID: str = ""
+    COGNITO_CLIENT_SECRET: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+settings = Settings(_env_file=".env", _env_file_encoding="utf-8")
